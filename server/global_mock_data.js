9c91458de5a76f907e3b1c73884eec3d68730677
KimJManansala <kimjmanansala@gmail.com>
2021-05-22 09:20:15 -0400

    backend setup

--- server/global_mock_data.js
+++ server/global_mock_data.js
@@ -17,25 +17,34 @@ const mock_messages_data = [
     {
         id: 1,
         channelId: 1,
-        message: 'Random message text'
+        message: 'Random message text',
     },
     {
         id: 2,
         channelId: 1,
-        message: 'Another message text'
+        message: 'Another message text',
     },
     {
         id: 3,
         channelId: 1,
-        message: 'We got Another message text!'
-    }
+        message: 'We got Another message text!',
+    },
     {
         id: 4,
-        channelId: 1,
-        message: 'We got Another message text!'
-    }
+        channelId: 2,
+        message: 'Channel 2 first message!',
+    },
+    {
+        id: 5,
+        channelId: 2,
+        message: 'Channel 2 Second message!',
+    },
 ]
 
-export {
 
+module.exports = {
+    mock_channels_data,
+    mock_messages_data,
 }
+
+
