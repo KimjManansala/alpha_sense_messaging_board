02919716e50afef477c38c7e714e42d61bc91bc3
KimJ Manansala <40606399+KimjManansala@users.noreply.github.com>
2021-05-23 18:54:57 -0400

    component name updated (#12)

---
+++ client/src/views/MessageBoard/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Card, CardBody, CardText} from "reactstrap";
+
+function LandingPage() {
+    return (
+        <Card>
+            <CardBody>
+                <h2>Welcome to this mock storyboard!</h2>
+                <h3>Please go to the navigation tab to browse the channels!</h3>
+            </CardBody>
+        </Card>
+    );
+}
+
+export default LandingPage;
